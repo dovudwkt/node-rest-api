190e5d522118fcc7d20cd914425dc7fa7d52a41e
Dovud Inomov <dinomov99@gmail.com>
2020-09-21 17:11:44 +0300

    Modified API response structure

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,13 +6,27 @@ const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
   Product.find()
+    .select("name price _id") // Select only listed fields
     .exec()
     .then((data) => {
-      console.log(data);
-      res.status(200).json(data);
+      const result = {
+        count: data.length,
+        products: data.map((d) => {
+          return {
+            name: d.name,
+            price: d.price,
+            _id: d._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/products/" + d._id,
+            },
+          };
+        }),
+      };
+
+      res.status(200).json(result);
     })
     .catch((err) => {
-      console.log(err);
       res.status(500).json({
         error: err,
       });
@@ -29,10 +43,16 @@ router.post("/", (req, res, next) => {
   product
     .save()
     .then((result) => {
-      console.log(result);
       res.status(201).json({
-        message: "Handling POST requests to /products",
-        createdProduct: result,
+        message: "Product created",
+        createdProduct: {
+          _id: result._id,
+          name: result.name,
+          request: {
+            type: "GET",
+            url: "http://localhost:3000/products/" + result._id,
+          },
+        },
       });
     })
     .catch((err) => {
@@ -46,9 +66,9 @@ router.post("/", (req, res, next) => {
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
   Product.findById(id)
+    .select("name price _id")
     .exec()
     .then((data) => {
-      console.log(data);
       res.status(200).json(data);
     })
     .catch((err) => {
@@ -70,8 +90,11 @@ router.patch("/:productId", (req, res, next) => {
   Product.update({ _id: id }, updateOps)
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({
+        message: "Product updated",
+        _id: id,
+        request: { type: "GET", url: "http://localhost:3000/products/" + id },
+      });
     })
     .catch((err) => {
       consosle.log(err);
@@ -83,8 +106,7 @@ router.delete("/:productId", (req, res, next) => {
   Product.remove({ _id: id })
     .exec()
     .then((result) => {
-      console.log(result);
-      res.status(200).json(result);
+      res.status(200).json({ message: "Product deleted" });
     })
     .catch((err) => {
       console.log(err);
