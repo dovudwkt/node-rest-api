a30e52dfe3b5700de73d9dd455d3c035ce2420a9
Dovud Inomov <dinomov99@gmail.com>
2020-09-21 10:54:16 +0300

    CORS

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const morgan = require('morgan'); // Middleware for logs
-const bodyParser = require('body-parser');
+const morgan = require("morgan"); // Middleware for logs
+const bodyParser = require("body-parser");
 
 // Routes
-const productRoutes = require('./api/routes/products');
+const productRoutes = require("./api/routes/products");
 // const orderRoutes = require('./api/routes/orders');
 
-
 // Run logging middleware before other routes
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}) ); // see docs
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false })); // see docs
 app.use(bodyParser.json());
 
-// Routes which should handle requests
-app.use('/products', productRoutes);
-// app.use('/orders', orderRoutes);
+// CORS
+app.use((req, res, next) => {
+  res.header("Access-Controll-Allow-Origin", "*");
+  res.header(
+    "Access-Controll-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header(
+      "Access-Controll-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
 
+// Routes which should handle requests
+app.use("/products", productRoutes);
+// app.use('/orders', orderRoutes);
 
 // Handle errors. If no routes found, then the following line is reached
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
-})
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-        error: {
-            message: error.message
-        }
-    });
-})
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 module.exports = app;
